9bfdda6826f7e246656416623b1a2d887a1a60b4
GOAT-12 <arustidekpandja@gmail.com>
2025-12-15 22:05:42 +0000

    feat: amélioration du composant About avec une meilleure réactivité

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -19,6 +19,14 @@ module.exports = {
     "./node_modules/astro-icon/**/*.js"
   ],
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         'nvim-bg': '#1c1c1c',
